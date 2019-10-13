1a72bd7521e52e65cf53bdb3bb15b522b1f9b282
Kaura Arja <s1900556@edu.bc.fi>
2019-10-13 15:26:14 +0300

    reduxdemo

---
+++ src/actions.js
@@ -0,0 +1,11 @@
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
+export const ADD = 'ADD';
+export const REMOVE = 'REMOVE';
+export const STORE_RESULT = 'STORE_RESULT';
+export const DELETE_RESULT = 'DELETE_RESULT';
+
+
+
+
+
